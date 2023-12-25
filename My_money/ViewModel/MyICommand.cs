8f0fc4fb53e84f880d8c2ce0988853c646cb86c4
zabro <zabro@DESKTOP-OEGATFJ>
2023-12-26 00:16:35 +0100

    add MyICommand

---
+++ My_money/ViewModel/MyICommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace My_money.ViewModel
+{
+    class MyICommand : ICommand
+    {
+        Action _TargetExecuteMethod;
+        Func<bool> _TargetCanExecuteMethod;
+
+        public MyICommand(Action executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public MyICommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+
+            if (_TargetCanExecuteMethod != null)
+            {
+                return _TargetCanExecuteMethod();
+            }
+
+            if (_TargetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+      public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter)
+        {
+            if (_TargetExecuteMethod != null)
+            {
+                _TargetExecuteMethod();
+            }
+        }
+    }
+}
