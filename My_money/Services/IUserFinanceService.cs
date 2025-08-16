6ca2e88cc18cafa4f058a36948eabe7056bfdd06
Maksim Zabrodin <zabro@DESKTOP-OEGATFJ>
2025-08-16 23:41:52 +0200

    Added UserFinanceService

---
+++ My_money/Services/IUserFinanceService.cs
@@ -0,0 +1,13 @@
+﻿
+using My_money.Model;
+using System.Threading.Tasks;
+
+namespace My_money.Services
+{
+    public interface IUserFinanceService
+    {
+        Task<UserFinance> GetUserFinanceAsync();
+        Task AddUserFinanceAsync(UserFinance userFinance);
+        Task UpdateUserFinanceAsync(float savings, float balance);
+    }
+}
---
+++ My_money/Services/UserFinanceService.cs
@@ -0,0 +1,53 @@
+﻿using My_money.Data.Repositories;
+using My_money.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace My_money.Services
+{
+    public class UserFinanceService : IUserFinanceService
+    {
+        private readonly IUserFinanceRepository _userFinanceRepository;
+
+        public UserFinanceService(IUserFinanceRepository userFinanceRepository)
+        {
+            _userFinanceRepository = userFinanceRepository;
+        }
+
+        public async Task AddUserFinanceAsync(UserFinance userFinance)
+        {
+            _ = userFinance ?? throw new ArgumentNullException(nameof(userFinance), "User finance cannot be null");
+
+            await _userFinanceRepository.AddAsync(userFinance);
+        }
+
+        public async Task<UserFinance> GetUserFinanceAsync()
+        {
+            return await _userFinanceRepository.GetAsync() ?? await AddDefaultUserFinance();
+        }
+
+        public async Task UpdateUserFinanceAsync(float savings, float balance)
+        {
+            _ = savings >= 0 ? savings : throw new ArgumentOutOfRangeException(nameof(savings), "Savings cannot be negative");
+
+            await _userFinanceRepository.UpdateAsync(new UserFinance
+            {
+                Savings = savings,
+                Balance = balance
+            });
+        }
+
+        private async Task<UserFinance> AddDefaultUserFinance()
+        {
+            var defaultFinance = new UserFinance
+            {
+                Savings = 0,
+                Balance = 0
+            };
+
+            await _userFinanceRepository.AddAsync(defaultFinance);
+            return defaultFinance;
+
+        }
+    }
+}
